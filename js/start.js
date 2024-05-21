aedceb10c9e62961ea5071a8848bab04fdeb4139
6921min <a01022676921@gmail.com>
2024-05-21 22:19:51 +0900

    야근작

--- js/start.js
+++ js/start.js
@@ -1,7 +1,21 @@
 const main=document.querySelector("#main");
 const qna=document.querySelector("#qna");
 
+function gonext(){
+    var q=document.querySelector('.qbox');
+    q.innerHTML=qnalist[0].q;
+}
 function begin(){
-    main.style.display="none";
-    qna.style.display="block";
+    main.style.WebkitAnimation="fadeout 1s";
+    main.style.animation="fadeout 1s";
+    
+    setTimeout(() => {
+        qna.style.display="block";
+        qna.style.WebkitAnimation="fadein 1s";
+        qna.style.animation="fadein 1s";
+        setTimeout(() => {main.style.display="none",qna.style.display="block";},450)
+    },gonext(); 450);
+
+    //main.style.display="none";
+    //qna.style.display="block";
 }
