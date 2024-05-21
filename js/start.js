8cf20a88d0b2aa079deba29a0704d79fdc14e02a
6921min <a01022676921@gmail.com>
2024-05-21 22:51:28 +0900

    어팬드차일드가 뭔가 하자있는듯 함. 정의 된 적 없다는데 확장문제아닐까

--- js/start.js
+++ js/start.js
@@ -1,9 +1,18 @@
 const main=document.querySelector("#main");
 const qna=document.querySelector("#qna");
 
-function gonext(){
+function addanswer(answertext){
+    var a=document.querySelectorAll('.answerbox');
+    var answer=document.createElement('button');
+    a.appendChild(answer);
+    answer.innerHTML=answertext;
+}
+function gonext(qidx){
     var q=document.querySelector('.qbox');
-    q.innerHTML=qnalist[0].q;
+    q.innerHTML=qnalist[qidx].q;
+    for(let i in qnalist[qidx].a){
+        addanswer(qnalist[qidx].a[i].answer);
+    }
 }
 function begin(){
     main.style.WebkitAnimation="fadeout 1s";
@@ -14,8 +23,7 @@ function begin(){
         qna.style.WebkitAnimation="fadein 1s";
         qna.style.animation="fadein 1s";
         setTimeout(() => {main.style.display="none",qna.style.display="block";},450)
-    },gonext(); 450);
-
-    //main.style.display="none";
-    //qna.style.display="block";
+        let qidx=0;
+        gonext(qidx);
+    },450);
 }
