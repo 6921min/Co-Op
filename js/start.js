32ee07296800300a74780abd51168444878e58f4
6921min <a01022676921@gmail.com>
2024-05-26 18:46:19 +0900

    초기화하고 다시굴림. 하자땜시 애가 얼어서 암것도 출력안함

--- js/start.js
+++ js/start.js
@@ -1,37 +1,17 @@
-const main=document.querySelector("#main");
-const qna=document.querySelector("#qna");
+const main = document.querySelector("#main");
+const qna = document.querySelector("#qna");
 
-function addAnswer(answerText,qIdx){
-    var a=document.querySelector('.answerBox');
-    var answer=document.createElement('button');
-    answer.classList.add('answerList');
-    a.appendChild(answer);
-    answer.innerHTML=answerText;
-    
-    answer.addEventListener("click",function(){
-        var children = document.querySelectorAll('.answerList');
-        for(let i=0;i<children.length;i++){
-            children[i].disabled=true;
-            children[i].style.display='none';
-        }
-    },false);
-    
-}
-function goNext(qIdx){
-    var q=document.querySelector('q.Box');
+function goNext(){
+    var q=document.querySelector('.qBox');
     q.innerHTML=qnaList[0].q;
-    
 }
+
 function begin(){
-    main.style.WebkitAnimation="fadeout 1s";
-    main.style.animation="fadeout 1s";
-    
     setTimeout(() => {
-        qna.style.display="block";
-        qna.style.WebkitAnimation="fadein 1s";
-        qna.style.animation="fadein 1s";
-        setTimeout(() => {main.style.display="none",qna.style.display="block";},450)
-        let qIdx=0;
-        goNext(qIdx);
+        //에니메이션
+        //애니메이션
+        setTimeout(() => {//애니메이션//애니메이션
+        },450);
+        goNext();
     },450);
 }
