e05655d7f9cf9ee2acfbd6631baa4e4654f05a1b
6921min <a01022676921@gmail.com>
2024-05-26 19:18:09 +0900

    qna에서 가림처리로 말아먹은 부분 처리됐고, 스타트 js 처음부터 다시 갈아 엎음. 자동 완성 쓸때 항상 조심해야겠다 싶은부분. 적응 안되니까 오죽 답답하다

--- js/start.js
+++ js/start.js
@@ -1,17 +1,41 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
-function goNext(){
+function addAnswer(answerText,qIdx) {
+    var a=document.querySelector('.answerBox');
+    var answer=document.createElement('button');
+    answer.classList.add('answerList');
+    a.appendChild(answer);
+    answer.innerHTML=answerText;
+    
+    answer.addEventListener("click",function(){
+        var children = document.querySelectorAll('.answerList');
+        for(let i=0;i<children.length;i++){
+            children[i].disabled='true';
+            children[i].style.display='none';
+        }
+        goNext(++qIdx);
+    },false);
+}
+
+function goNext(qIdx){
     var q=document.querySelector('.qBox');
-    q.innerHTML=qnaList[0].q;
+    q.innerHTML=qnaList[qIdx].q;
+    for(let i in qnaList[qIdx].a){
+        addAnswer(qnaList[qIdx].a[i].answer,qIdx);
+    }
 }
 
 function begin(){
     setTimeout(() => {
         //에니메이션
         //애니메이션
-        setTimeout(() => {//애니메이션//애니메이션
+        setTimeout(() => {
+            
+            main.style.display="none";
+            qna.stlye.display="block";
         },450);
-        goNext();
+        let qIdx=0;
+        goNext(qIdx);
     },450);
 }
